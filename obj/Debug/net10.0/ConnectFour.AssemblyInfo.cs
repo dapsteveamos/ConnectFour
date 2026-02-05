1e0424535724b1eec9ff5ac63f11a2d4e936b8ef
Steve Amos Daprim <dapsteveamos@gmail.com>
2026-02-05 15:57:24 +0000

    Update 6

--- obj/Debug/net10.0/ConnectFour.AssemblyInfo.cs
+++ obj/Debug/net10.0/ConnectFour.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+799426ef1ca506636a3c6bfb2a0f30eefa29a789")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41f89132ae55932e5d4424fc0a2d21d7094115d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
